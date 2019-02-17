cf7dd0cdd5161c462e85502817a507a4edbe7636
ClementDelgrange <clement.delgrange@hotmail.fr>
2019-02-17 20:44:29 +0100

    Cloud function triggered by a cloud storage event

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from pathlib import Path
-import logging
 import io
+import datetime
+import uuid
+import logging
 
-import flask
-from google.cloud import storage
 import rasterio
+import flask
+
+from google.cloud import storage
+from google.cloud import bigquery
 
 
 storage_client = storage.Client()
+bigquery_client = bigquery.Client()
+
 
 ALLOWED_EXTENSIONS = {".png"}
 
@@ -73,7 +79,37 @@ def get_raster_stat(request):
         logging.error("A parameter is missing", exc_info=True)
         flask.abort(400)
 
-    with rasterio.open(f"gs://{bucket}/{filename}") as src:
-        profile = src.profile
+    with rasterio.Env():
+        with rasterio.open(f"gs://{bucket}/{filename}") as src:
+            profile = src.profile
 
     return flask.jsonify(profile)
+
+
+def register_gcs_upload(data, context):
+    """
+    Background Cloud Function to be triggered by Cloud Storage.
+    This function adds an entry in a BigQuery table with the filename and the date.
+    The event metadata are written to Stackdriver Logging.
+
+    :param data: the Cloud Functions event payload
+    :type  data: dict
+    :param context: metadata of triggering event
+    :type context: google.cloud.functions.Context
+    """
+    logging.info(f"Event data: {data}")
+    filename = data["name"]
+    query = f"INSERT INTO storage_activity.file_upload (id, name, date) VALUES (@id, @name, @date);"
+    params = [
+        bigquery.ScalarQueryParameter("id", "STRING", str(uuid.uuid4())),
+        bigquery.ScalarQueryParameter("name", "STRING", filename),
+        bigquery.ScalarQueryParameter("date", "DATETIME", datetime.datetime.now())
+    ]
+    job_config = bigquery.QueryJobConfig()
+    job_config.query_parameters = params
+
+    res = bigquery_client.query(query, job_config=job_config)
+    if res.errors:
+        logging.error(f"Unable to insert the row in BigQuery: {res.query} / {res.query_parameters}")
+    else:
+        logging.info(f"Row inserted in BigQuery: {res.query} / {res.query_parameters}")
